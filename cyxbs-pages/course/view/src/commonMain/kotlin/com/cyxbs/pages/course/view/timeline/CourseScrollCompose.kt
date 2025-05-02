786ea083e8484e66b7c6167bc5e194b9bfdc8942
985892345 <guo985892345@foxmail.com>
2025-05-03 00:20:30 +0800

    :sparkles: 课表支持多指时的上下滚动

--- cyxbs-pages/course/view/src/commonMain/kotlin/com/cyxbs/pages/course/view/timeline/CourseScrollCompose.kt
+++ cyxbs-pages/course/view/src/commonMain/kotlin/com/cyxbs/pages/course/view/timeline/CourseScrollCompose.kt
@@ -3,7 +3,11 @@ package com.cyxbs.pages.course.view.timeline
 import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.MutatePriority
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
@@ -11,14 +15,22 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.changedToUpIgnoreConsumed
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.util.fastFirstOrNull
 import androidx.compose.ui.util.fastForEach
 import androidx.compose.ui.util.fastMapIndexed
 import com.cyxbs.components.utils.compose.reflexScrollableForMouse
+import com.cyxbs.components.utils.extensions.logg
 import com.cyxbs.pages.course.view.timeline.data.MutableTimelineData
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.collectLatest
 import kotlin.math.roundToInt
 
@@ -39,7 +51,8 @@ internal fun CourseScrollCompose(
   Layout(
     modifier = modifier
       .reflexScrollableForMouse()
-      .verticalScroll(state = verticalScrollState),
+      .verticalScroll(state = verticalScrollState)
+      .multiPointerScroll(verticalScrollState),
     content = { content() },
     measurePolicy = remember(timeline) {
       { measurables, constraints ->
@@ -55,8 +68,9 @@ internal fun CourseScrollCompose(
         val topPadding = scrollPaddingValues.calculateTopPadding().roundToPx()
         val bottomPadding = scrollPaddingValues.calculateBottomPadding().roundToPx()
         // 因为有 verticalScroll，所以这里 minHeight 就是父布局的高度
-        val height = ((constraints.minHeight - topPadding - bottomPadding) * (nowWeight / initialWeight))
-          .roundToInt()
+        val height =
+          ((constraints.minHeight - topPadding - bottomPadding) * (nowWeight / initialWeight))
+            .roundToInt()
         val maxWidth = constraints.maxWidth - startPadding - endPadding
         val placeables = measurables.fastMapIndexed { index, measurable ->
           measurable.measure(
@@ -91,6 +105,55 @@ internal fun CourseScrollCompose(
               scrollBy((verticalScrollState.maxValue - verticalScrollState.value).toFloat())
             }
           }
+        }
+      }
+    }
+  }
+}
+
+
+// verticalScroll 只支持首个手指的滚动，该方法实现非首个手指的滚动（比如首个手指被子节点消费时，其他手指仍可以进行滚动）
+// 位置需要放置在 verticalScroll 之后
+@Stable
+@Composable
+private fun Modifier.multiPointerScroll(verticalScrollState: ScrollState): Modifier {
+  val chanel = remember { Channel<Float>(capacity = Channel.UNLIMITED) }
+  LaunchedEffect(verticalScrollState) {
+    while (true) {
+      val value = chanel.receive()
+      try {
+        verticalScrollState.scroll(MutatePriority.UserInput) {
+          scrollBy(-value)
+        }
+      } catch (e: CancellationException) {
+        // 可能因为 scroll 优先级而被取消
+      }
+    }
+  }
+  return pointerInput(Unit) {
+    awaitEachGesture {
+      var main = awaitFirstDown(requireUnconsumed = false)
+      while (true) {
+        val event = awaitPointerEvent()
+        val mainPointer = event.changes.fastFirstOrNull { it.id == main.id }
+        if (mainPointer == null) break
+        if (mainPointer.changedToUpIgnoreConsumed()) {
+          // 这里按照 Scrollable -> DragGestureNode -> detectDragGestures -> awaitPointerSlopOrCancellation 逻辑
+          // 如果第一根手指抬起来则会去选择下一根处于 pressed 的手指
+          main = event.changes.fastFirstOrNull { it.pressed } ?: break
+        }
+        // 因为节点在 verticalScroll 之后，所以会先收到事件分发的 Main 流程
+        // 在首个 pointer 未消费时，verticalScroll 可能就会触发他的消费然后进行滚动
+        // 但 verticalScroll 只支持首个 pointer 的消费，所以我们在首个 pointer 被子节点消费时才允许其他手指的滚动
+        // 防止首个 pointer 与其他 pointer 都参与了滚动
+        if (!mainPointer.isConsumed) continue
+        if (event.changes.size == 1) continue
+        val pointer =
+          event.changes.fastFirstOrNull { it.id != main.id && !it.isConsumed && it.pressed }
+        if (pointer != null) {
+          val positionChange = pointer.positionChange()
+          pointer.consume()
+          chanel.trySend(positionChange.y)
         }
       }
     }
