b669193fa3ef5bdde0c4ee1feef294a04f729c5c
985892345 <guo985892345@foxmail.com>
2025-10-04 12:46:39 +0800

    :bug: BottomNavViewModel 在存在红点时重新登陆崩溃

--- cyxbs-pages/home/src/mobileMain/kotlin/com/cyxbs/pages/home/mobile/viewmodel/BottomNavViewModel.kt
+++ cyxbs-pages/home/src/mobileMain/kotlin/com/cyxbs/pages/home/mobile/viewmodel/BottomNavViewModel.kt
@@ -22,6 +22,7 @@ import cyxbsmobile.cyxbs_pages.home.generated.resources.home_nav_mine
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.emptyFlow
@@ -37,16 +38,22 @@ import org.jetbrains.compose.resources.StringResource
  */
 class BottomNavViewModel : BaseViewModel() {
 
+  val height: Dp = 60.dp
+  val offsetYRadio: MutableFloatState = mutableFloatStateOf(0F)
+  val alpha: MutableFloatState = mutableFloatStateOf(1F)
+
   val discoverItem = BottomNavItem(
     Res.string.home_nav_discover,
     Res.drawable.home_ic_explore_selected,
     Res.drawable.home_ic_explore_unselected,
   )
+
   val fairgroundItem = BottomNavItem(
     Res.string.home_nav_fairground,
     Res.drawable.home_ic_fairground_selectored,
     Res.drawable.home_ic_fairground_unselectored,
   )
+
   val mineItem = BottomNavItem(
     Res.string.home_nav_mine,
     Res.drawable.home_ic_mine_selected,
@@ -56,13 +63,7 @@ class BottomNavViewModel : BaseViewModel() {
   )
 
   val items = persistentListOf(discoverItem, fairgroundItem, mineItem)
-
-  val height: Dp = 60.dp
-
   val selectedItem: MutableStateFlow<BottomNavItem> = MutableStateFlow(items[0])
-
-  val offsetYRadio: MutableFloatState = mutableFloatStateOf(0F)
-  val alpha: MutableFloatState = mutableFloatStateOf(1F)
 
   fun select(item: BottomNavItem) {
     selectedItem.value = item
@@ -78,13 +79,17 @@ class BottomNavViewModel : BaseViewModel() {
     val redObserveFlow: Flow<Boolean> = emptyFlow(), // 监听红点状态的 Job
   ) {
 
-    private val redJob = redObserveFlow.distinctUntilChanged().collectLaunch {
-      if (it && selectedItem.value !== this) {
-        redDot.value = true
+    private val redDot = MutableStateFlow(false)
+
+    // redObserveFlow 的监听需要使用到 selectedItem，
+    // 如果直接以类变量初始化，则此时 selectedItem 还没有初始化，会导致空指针
+    private val redJob by lazy {
+      redObserveFlow.distinctUntilChanged().collectLaunch {
+        if (it && selectedItem.value !== this) {
+          redDot.value = true
+        }
       }
     }
-
-    private val redDot = MutableStateFlow(false)
 
     fun select() {
       if (redDot.value) {
@@ -93,6 +98,9 @@ class BottomNavViewModel : BaseViewModel() {
       }
     }
 
-    fun observerRedDot() = redDot.asStateFlow()
+    fun observerRedDot(): StateFlow<Boolean> {
+      redJob.hashCode() // 触发第一次初始化
+      return redDot.asStateFlow()
+    }
   }
 }
