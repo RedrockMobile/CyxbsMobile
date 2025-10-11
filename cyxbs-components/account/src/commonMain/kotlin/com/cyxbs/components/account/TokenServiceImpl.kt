86429f324c68cb3c57d5852af89b4096218b9a2e
985892345 <guo985892345@foxmail.com>
2025-10-11 22:21:52 +0800

    :zap: 优化 token 刷新机制，使其能提前异步触发刷新操作

--- cyxbs-components/account/src/commonMain/kotlin/com/cyxbs/components/account/TokenServiceImpl.kt
+++ cyxbs-components/account/src/commonMain/kotlin/com/cyxbs/components/account/TokenServiceImpl.kt
@@ -24,6 +24,8 @@ import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
+import kotlinx.atomicfu.AtomicBoolean
+import kotlinx.atomicfu.atomic
 import kotlinx.atomicfu.locks.SynchronizedObject
 import kotlinx.atomicfu.locks.synchronized
 import kotlinx.coroutines.Deferred
@@ -36,6 +38,7 @@ import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 import kotlin.concurrent.Volatile
 import kotlin.time.Clock
+import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
 
@@ -50,18 +53,34 @@ object TokenServiceImpl : ITokenService {
 
   private val mutex = Mutex()
 
+  // 是否在异步刷新 token 中
+  private val isInAsyncRefresh: AtomicBoolean = atomic(false)
+
   override suspend fun getOrRequestToken(): String? {
     val token = TokenProvider.stateFlow.value ?: return null  // 未登录则直接返回 null
-    if (!TokenProvider.isTokenExpired()) {
-      // 未过期则直接返回 token
+    val tokenRemainTime = TokenProvider.getTokenRemainTime()
+    if (tokenRemainTime > 12.hours) {
+      // 大于 12 小时的 token 不需要触发刷新
       return token.token
-    }
-    return mutex.withLock {
-      if (token != TokenProvider.stateFlow.value) {
-        // 已经请求成功
-        token.token
-      } else {
-        requestToken(token).await()
+    } else if (tokenRemainTime > 10.minutes) {
+      // 12 小时 - 10 分钟内 则异步触发 token 的刷新
+      if (!isInAsyncRefresh.compareAndSet(expect = false, update = true)) {
+        // 这里竞争不会很激烈，采取自旋而非协程锁的挂起
+        requestToken(token).invokeOnCompletion {
+          isInAsyncRefresh.lazySet(false)
+        }
+      }
+      return token.token
+    } else {
+      // 10 分钟后或已经过期则同步触发 token 的刷新
+      return mutex.withLock {
+        if (token != TokenProvider.stateFlow.value) {
+          // 如果 token 不相等，则说明 token 已经刷新了
+          token.token
+        } else {
+          // 同步等待刷新
+          requestToken(token).await()
+        }
       }
     }
   }
