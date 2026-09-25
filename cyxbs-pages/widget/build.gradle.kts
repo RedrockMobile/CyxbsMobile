25794a29a3230d40351da31e3c3ed3c81042548e
985892345 <guo985892345@foxmail.com>
2026-09-26 01:04:02 +0800

    :arrow_up: 修改 Config 中的版本号

--- cyxbs-pages/widget/build.gradle.kts
+++ cyxbs-pages/widget/build.gradle.kts
@@ -16,13 +16,11 @@ kotlin {
       implementation(projects.cyxbsComponents.view)
       implementation(projects.cyxbsComponents.utils)
       implementation(projects.cyxbsPages.widget.api)
+      implementation(projects.cyxbsPages.course.api)
       implementation(libs.kotlinx.coroutines)
       implementation(libs.kotlinx.datetime)
     }
     androidMain.dependencies {
-      // Activity 只依赖课程对外详情协议，具体数据查询与 BottomSheet 内容由 course 实现提供。
-      implementation(projects.cyxbsComponents.config)
-      implementation(projects.cyxbsPages.course.api)
       implementation(libs.compose.activity)
       implementation(libs.androidx.glance)
       implementation(libs.androidx.glance.appwidget)
